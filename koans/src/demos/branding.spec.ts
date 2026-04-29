85659148882d26f18b6fb180e40fcfd3f3ea3349
Jeffry Gonzalez <jeff@hypertheory.com>
2026-04-29 14:43:43 -0400

    done with types

--- koans/src/demos/branding.spec.ts
+++ koans/src/demos/branding.spec.ts
@@ -1,15 +1,49 @@
-import { describe, it } from "vitest";
-import { transferEmployee } from "./hr";
+import { describe, it } from 'vitest';
+import {
+  assignDepartment,
+  getDepartments,
+  getEmployeeById,
+  giveEmployeeRaise,
+  hire,
+  saveNewEmployee,
+  transferEmployee,
+  transferEmployee2,
+} from './hr';
+import { isNone, isSome } from './hr/option';
+import { isSuccess } from './hr/results';
 
-describe("Branded Types", () => {
+describe('Branded Types', () => {
+  it('example 1 leadup', () => {
+    // This is *WAY* More about 'making impossible states possible'
+    // than any particular pattern.
 
-    it('example 1 leadup', () => {
-     
+    const employeeOption = getEmployeeById('9932');
+    
+    if (isSome(employeeOption)) {
+      const employee = employeeOption.value;
+      const departments = getDepartments();
+      const department = departments[0]!;
 
+      transferEmployee2(department, employee);
 
-        const employeeId = '123-89';
-        const departmentId = 'DEV';
+      giveEmployeeRaise(employee);
 
-        transferEmployee(departmentId, employeeId);
-    })
+      const chris = hire('Chris');
+
+      const chris2 = assignDepartment(chris, department);
+
+      if(isSuccess( saveNewEmployee(chris2))) {
+        // saved the employee just fine!
+        
+      } else {
+        // did not save
+      }
+    }
+
+    if(isNone(employeeOption)) {
+        console.log('No Employee with that id!');
+    }
+  });
 });
+
+describe('what about errors?', () => {});
--- koans/src/demos/hr/index.ts
+++ koans/src/demos/hr/index.ts
@@ -1,12 +1,64 @@
-export function getEmployeeById(id: string) {
-  return {
+import { fail } from "assert";
+import { none, Option, some } from "./option";
+import { Results, succeeded } from "./results";
+
+declare const brand: unique symbol;
+export type Brand<T, TBrand> = T & { [brand]: TBrand}
+
+
+
+type Employee = {
+    id: string;
+    name: string;
+}
+type Department = {
+    id: string;
+};
+type BrandedEmployee = Brand<Employee, "Employee">;
+type BrandedDepartment = Brand<Department, "Department">;
+
+export function getEmployeeById(id: string): Option<BrandedEmployee> {
+  if(id === '999') {
+    return none;
+  }
+  return some({
     id,
     name: 'Test Employee',
-  };
+  } as BrandedEmployee);
 }
 
-export function getDepartments() {
-  return [{ id: 'Dev' }, { id: 'QA' }, { id: 'Sales' }];
+export function getDepartments(): BrandedDepartment[] {
+  return [{ id: 'Dev' } as BrandedDepartment, { id: 'QA' } as BrandedDepartment, { id: 'Sales' } as BrandedDepartment]
 }
 
-export function transferEmployee(employeeId: string, departmentId: string) {}
+export function transferEmployee(
+  employeeId: string, 
+  departmentId: string) {
+    // do the transfer.
+  }
+
+  export function transferEmployee2(dept:BrandedDepartment, emp:BrandedEmployee) {
+
+  }
+
+export function giveEmployeeRaise(emp:BrandedEmployee) {
+
+}
+
+type HiringRequest = Brand<Employee, 'EmployeeHiringRequest'>;
+export function hire(name:string) {
+    return {
+      id: crypto.randomUUID(), // uuid
+      name
+    } as HiringRequest;
+}
+
+type AssignedEmployee = Brand<Employee, "EmployeeAssignedDepartment">;
+export function assignDepartment(emp:HiringRequest, dep: BrandedDepartment) {
+  // do something.
+  return emp as unknown as AssignedEmployee;
+}
+export function saveNewEmployee(emp: AssignedEmployee ) : Results<string> {
+  //return fail('Did not work')
+  return succeeded("did it");
+}
